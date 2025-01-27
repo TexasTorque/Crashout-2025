601f6598f7667f8316b581a29ef6460c95ba228f
humandavey <davey.adams.three@gmail.com>
2025-01-27 16:13:00 -0600

    feat: add new auto

--- src/main/java/org/texastorque/auto/AutoManager.java
+++ src/main/java/org/texastorque/auto/AutoManager.java
@@ -21,6 +21,11 @@ public final class AutoManager extends TorqueAutoManager {
                 new ReefSequence(Location.CENTER, Location.FAR, EndAction.L3_L, EndAction.ALGAE_EXTRACTION_LOW),
                 new ReefSequence(Location.FAR, Location.PROCESSOR, EndAction.PROCESSOR)
         );
+        addBaseAuto("LFT -> L3 -> CSL -> L3",
+                new ReefSequence(Location.LEFT, Location.CLOSE_LEFT, EndAction.L3_L, EndAction.ALGAE_EXTRACTION_LOW),
+                new ReefSequence(Location.CLOSE_LEFT, Location.CORAL_STATION_LEFT, EndAction.CORAL_PICKUP),
+                new ReefSequence(Location.CORAL_STATION_LEFT, Location.CLOSE_LEFT, EndAction.ALGAE_EXTRACTION_LOW, EndAction.L3_R)
+        );
     }
 
     @Override
