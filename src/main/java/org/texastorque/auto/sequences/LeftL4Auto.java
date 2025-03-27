98e9a8a95c25c00b4f98b8a440e9e3c81d5ab837
timagez <t.gezalov98@gmail.com>
2025-03-27 09:31:26 -0500

    fix: left L4 update

--- src/main/java/org/texastorque/auto/sequences/LeftL4Auto.java
+++ src/main/java/org/texastorque/auto/sequences/LeftL4Auto.java
@@ -14,6 +14,8 @@ import org.texastorque.torquelib.auto.commands.TorqueWaitTime;
 import org.texastorque.torquelib.auto.commands.TorqueWaitUntil;
 import org.texastorque.torquelib.auto.marker.Marker;
 
+import edu.wpi.first.wpilibj.RobotBase;
+
 public class LeftL4Auto extends TorqueSequence implements Subsystems {
     
     public LeftL4Auto() {
@@ -53,10 +55,13 @@ public class LeftL4Auto extends TorqueSequence implements Subsystems {
         // Alignment
         addBlock(new Align(Relation.CENTER, AlignableTarget.CORAL_STATION, 1.2).command());
 
-        // Pickup coral from coral station
-        addBlock(new TorqueWaitTime(.5));
+        // Waits in real life but not in sim (since it'll never have a coral in sim)
+        if (RobotBase.isReal()) {
+            addBlock(new TorqueWaitUntil(() -> claw.hasCoral()));
+        } else {
+            addBlock(new TorqueWaitTime(.5));
+        }
         addBlock(new TorqueRun(() -> claw.setCoralState(Claw.CoralState.OFF)));
-
         // Drive coral station left to close left
         addBlock(new TorqueFollowPath("LL4_3", drivebase).withMarkers(
             new Marker(() -> {
@@ -86,8 +91,12 @@ public class LeftL4Auto extends TorqueSequence implements Subsystems {
         // Alignment
         addBlock(new Align(Relation.CENTER, AlignableTarget.CORAL_STATION, 1.2).command());
 
-        // Pickup coral from coral station
-        addBlock(new TorqueWaitTime(.5));
+        // Waits in real life but not in sim (since it'll never have a coral in sim)
+        if (RobotBase.isReal()) {
+            addBlock(new TorqueWaitUntil(() -> claw.hasCoral()));
+        } else {
+            addBlock(new TorqueWaitTime(.5));
+        }
         addBlock(new TorqueRun(() -> claw.setCoralState(Claw.CoralState.OFF)));
 
         // Drive coral station left to close left
@@ -112,26 +121,5 @@ public class LeftL4Auto extends TorqueSequence implements Subsystems {
             perception.setDesiredAlignTarget(AlignableTarget.NONE);
         }));
         addBlock(new Align(() -> perception.getAlignPose(), 1).command());
-
-        // // Move to algae setpoints
-        // addBlock(new TorqueRun(() -> {
-        //     elevator.setState(Elevator.State.ALGAE_REMOVAL_LOW);
-        //     claw.setState(Claw.State.ALGAE_EXTRACTION);
-        //     claw.setAlgaeState(Claw.AlgaeState.INTAKE);
-        // }));
-
-        // addBlock(new TorqueWaitUntil(() -> elevator.isNearState() && claw.isNearState()));
-
-        // // Alignment
-        // addBlock(new Align(Relation.CENTER, AlignableTarget.ALGAE_LOW, .75).command());
-
-        // addBlock(new TorqueRun(() -> claw.setAlgaeState(Claw.AlgaeState.OFF)));
-
-        // // Move back to avoid claw hitting reef poles
-        // addBlock(new TorqueRun(() -> {
-        //     perception.setRelation(Relation.NONE);
-        //     perception.setDesiredAlignTarget(AlignableTarget.NONE);
-        // }));
-        // addBlock(new Align(() -> perception.getAlignPose(), 1).command());
     }
 }
